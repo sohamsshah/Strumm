04ba25735faa80bd1fa6934d776246e9b2e0aad7
sohamsshah <sohamshah456@gmail.com>
2021-04-05 12:40:41 +0530

    add sorting

--- src/components/Categories/Products/ProductPage/ProductPage.jsx
+++ src/components/Categories/Products/ProductPage/ProductPage.jsx
@@ -5,19 +5,76 @@ import {useCart} from "../../../../context/cart-context"
 import {filterByInstrumentName} from "../../../../utils/dataFilter"
 import ProductsCard from "../ProductsCard/ProductsCard"
 import styles from "./ProductPage.module.css"
+import {useReducer} from "react"
+import {sortByFunction} from "./../../../../utils/dataFilter"
+
+const filterReducer = (state, action) => {
+  switch(action.type){
+    case "TOGGLE_INVENTORY":
+      return (state = {
+        ...state,
+        showInventoryAll: !state.showInventoryAll
+      });
+    case "TOGGLE_DELIVERY":
+      return (state = {
+        ...state,
+        showInventoryAll: !state.showInventoryAll
+      });
+    case "SORT":
+      return {
+        ...state,
+        sortBy: action.payload
+      };
+    default:
+      return state;
+  }
+}
 
 export default function ProductPage({instrument, ...rest}) {
     const {products, dispatch} = useCart();
     const product = filterByInstrumentName(products, instrument);
+    const [{showAllProducts, showFastDeliveryOnly, sortBy}, filterDispatch] = useReducer(filterReducer, {
+      showInventoryAll: true,
+      showFastDeliveryOnly: false,
+      sortBy: null
+    });
+    const sortedProduct = sortByFunction(product, sortBy);
+    console.log(sortedProduct, sortBy);
     return (
         <div>
             <Navbar />
             <div className={styles.pageLayout}>
             <div className={styles.productFilters}>
-            Hi
+            <div>
+            <fieldset>
+        <legend>Sort By</legend>
+        <label>
+          <input
+            type="radio"
+            name="sort"
+            onChange={() =>
+              filterDispatch({ type: "SORT", payload: "PRICE_HIGH_TO_LOW" })
+            }
+            checked={sortBy && sortBy === "PRICE_HIGH_TO_LOW"}
+          ></input>{" "}
+          Price - High to Low
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="sort"
+            onChange={() =>
+              filterDispatch({ type: "SORT", payload: "PRICE_LOW_TO_HIGH" })
+            }
+            checked={sortBy && sortBy === "PRICE_LOW_TO_HIGH"}
+          ></input>{" "}
+          Price - Low to High
+        </label>
+      </fieldset>           
+            </div>
             </div>
             <div className={styles.allProducts}>
-            {product.map(
+            {sortedProduct.map(
               (item) => (
                 <ProductsCard products={product} dispatch={dispatch} item={item}/>
               )
--- src/utils/dataFilter.js
+++ src/utils/dataFilter.js
@@ -10,3 +10,14 @@ export const filterByInstrumentType = (data, instrument, type) => {
     return data.filter((item => item.category.instrument === instrument)).filter((item) => item.category.type === type)
 }
 
+export const sortByFunction = (data, sortBy) => {
+    switch(sortBy){
+        case "PRICE_HIGH_TO_LOW":
+            return data.sort((a, b) => b["price"] - a["price"]);;
+        case "PRICE_LOW_TO_HIGH":
+            return data.sort((a, b) => a["price"] - b["price"]);;
+        default:
+            return data;       
+    }
+}
+
